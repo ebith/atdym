cf127bb58e1276a844cd0b84c831843e96fbc572
ebith <ebith.h@gmail.com>
2025-06-05 13:04:36 +0000

    add meta viewport

--- src/Html.tsx
+++ src/Html.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-
 const Html = (props) => {
   return (
     <html>
       <head>
         <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>{`Atdym${props.children.props.title ? ': ' + props.children.props.title : ''}`}</title>
         <base target="_blank" />
         <link href="/static/style.css" rel="stylesheet" />
