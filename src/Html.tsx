fac0901f10d6d4bbec9f8cb0028b4c1dad6ffce6
ebith <ebith.h@gmail.com>
2025-08-14 14:37:24 +0000

    fix phone number link in iOS Safari

--- src/Html.tsx
+++ src/Html.tsx
@@ -5,6 +5,7 @@ const Html = (props) => {
     <html>
       <head>
         <meta charSet="utf-8" />
+        <meta name="format-detection" content="telephone=no" />
         <meta name="description" content="Web app for read it later" />
         <link rel="alternate" href="https://github.com/ebith/atdym" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
